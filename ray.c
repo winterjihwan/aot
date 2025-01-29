47faf2760780917475f058cb003970acf048228d
Ji Hwan An <angeljihwan@gmail.com>
2025-01-29 23:26:16 +0900

    Minimap wip

--- ray.c
+++ ray.c
@@ -7,22 +7,24 @@
 #include "raylib.h"
 
 static void ray_render_grid(SDL_Renderer *renderer, Color c) {
-  SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, 128);
+  SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, 255);
 
-  for (int i = CELL_W; i < WINDOW_W; i += CELL_W) {
-    SDL_RenderDrawLine(renderer, i, 0, i + 1, WINDOW_H);
+  for (int i = MN_X + MN_W / CELL_COUNT_W; i < MN_X + MN_W;
+       i += MN_W / CELL_COUNT_W) {
+    SDL_RenderDrawLine(renderer, i, MN_Y, i, MN_H);
   }
 
-  for (int i = CELL_H; i < WINDOW_H; i += CELL_H) {
-    SDL_RenderDrawLine(renderer, 0, i, WINDOW_W, i + 1);
+  for (int i = MN_Y + MN_H / CELL_COUNT_H; i < MN_Y + MN_H;
+       i += MN_H / CELL_COUNT_H) {
+    SDL_RenderDrawLine(renderer, MN_X, i, MN_X, i);
   }
 }
 
-static void ray_fill_circle(SDL_Renderer *renderer, Circle *c) {
-  float r_squared = powf(c->r, 2.0);
-  for (float i = c->x - c->r; i < c->x + c->r; i++) {
-    for (float j = c->y - c->r; j < c->y + c->r; j++) {
-      if (powf(i - c->x, 2.0) + powf(j - c->y, 2.0) < r_squared) {
+static void ray_fill_circle(SDL_Renderer *renderer, Vec2 *pos, float r) {
+  float r_squared = powf(r, 2.0);
+  for (float i = pos->x - r; i < pos->x + r; i++) {
+    for (float j = pos->y - r; j < pos->y + r; j++) {
+      if (powf(i - pos->x, 2.0) + powf(j - pos->y, 2.0) < r_squared) {
         SDL_RenderDrawPoint(renderer, i, j);
       }
     }
@@ -50,17 +52,18 @@ static void ray_trace(SDL_Renderer *renderer, Vec2 *v2, float t, Color c) {
 }
 
 static void ray_render_map(SDL_Renderer *renderer) {
-  for (int i = 0; i < CELL_H_COUNT; i++) {
-    for (int j = 0; j < CELL_W_COUNT; j++) {
+  for (int i = 0; i < CELL_COUNT_H; i++) {
+    for (int j = 0; j < CELL_COUNT_W; j++) {
       Color *c = GET_CELL(i, j);
       if (c == NULL)
         continue;
 
       SDL_SetRenderDrawColor(renderer, c->r, c->g, c->b, 255);
-      SDL_RenderFillRect(renderer, &(SDL_Rect){.x = j * CELL_W,
-                                               .y = i * CELL_H,
-                                               .w = CELL_W,
-                                               .h = CELL_H});
+      SDL_RenderFillRect(renderer,
+                         &(SDL_Rect){.x = MN_X + j * MN_W / CELL_COUNT_W,
+                                     .y = MN_Y + i * MN_H / CELL_COUNT_H,
+                                     .w = MN_W / CELL_COUNT_W,
+                                     .h = MN_H / CELL_COUNT_H});
     }
   }
 };
@@ -81,7 +84,7 @@ void ray_poll_event(SDL_Event *event) {
       float t = THETA(player_direction);
       float x = player.x + VELOCITY * cos(t);
       float y = player.y + VELOCITY * sin(t);
-      if (!OOB(x, y)) {
+      if (!OOB(x, y) && GET_CELL_COOR(y, x) == NULL) {
         player = (Circle){.x = x, .y = y, .r = player.r};
       }
     }
@@ -90,18 +93,22 @@ void ray_poll_event(SDL_Event *event) {
       float t = THETA(player_direction);
       float x = player.x - VELOCITY * cos(t);
       float y = player.y - VELOCITY * sin(t);
-      if (!OOB(x, y)) {
+      if (!OOB(x, y) && GET_CELL_COOR(y, x) == NULL) {
         player = (Circle){.x = x, .y = y, .r = player.r};
       }
     }
   }
 }
 
-void ray_render(SDL_Renderer *renderer, SDL_Event *event) {
+void ray_render_minimap(SDL_Renderer *renderer) {
   ray_render_grid(renderer, COLOR_WHITE);
   ray_render_map(renderer);
-  ray_fill_circle(renderer, &player);
 
   Vec2 player_pos = {.x = player.x, .y = player.y};
+  ray_fill_circle(renderer, &player_pos, player.r);
   ray_trace(renderer, &player_pos, THETA(player_direction), COLOR_YELLOW);
+}
+
+void ray_render(SDL_Renderer *renderer, SDL_Event *event) {
+  ray_render_minimap(renderer);
 }
--- raylib.h
+++ raylib.h
@@ -1,28 +1,42 @@
 #ifndef RAYLIB_H
 #define RAYLIB_H
 
+// Game
 #define FPS 30
 #define WINDOW_W 500
 #define WINDOW_H 500
 
-#define CELL_W 100
-#define CELL_H 100
-#define CELL_W_COUNT (WINDOW_W / CELL_W)
-#define CELL_H_COUNT (WINDOW_H / CELL_H)
-#define CELL_COUNT (CELL_W_COUNT * CELL_H_COUNT)
-#define GET_CELL(y, x) RAY_MAP[y * CELL_W_COUNT + x]
-#define GET_CELL_COOR(y_coor, x_coor)                                          \
-  RAY_MAP[(int)y_coor / 100 * CELL_W_COUNT + (int)x_coor / 100]
+// Minimap
+#define MN_SCALE 5
+#define MN_X 50
+#define MN_Y 50
+#define MN_H (WINDOW_W / MN_SCALE)
+#define MN_W (WINDOW_H / MN_SCALE)
 
+// Color
 #define COLOR_WHITE (Color){.r = 255, .g = 255, .b = 255}
 #define COLOR_BLACK (Color){.r = 0, .g = 0, .b = 0}
 #define COLOR_YELLOW (Color){.r = 255, .g = 252, .b = 127}
 
-#define THETA(n) (n / 360.0 * 2 * M_PI)
-#define OOB(x, y) (x < 0 || x > WINDOW_W || y < 0 || y > WINDOW_H)
-#define NCP 1
-#define VELOCITY 5
+// Cell
+#define CELL_W 100
+#define CELL_H 100
+#define CELL_COUNT_W (WINDOW_W / CELL_W)
+#define CELL_COUNT_H (WINDOW_H / CELL_H)
+#define CELL_COUNT (CELL_COUNT_W * CELL_COUNT_H)
+#define GET_CELL(y, x) RAY_MAP[y * CELL_COUNT_W + x]
+#define GET_CELL_COOR(y_coor, x_coor)                                          \
+  RAY_MAP[(int)y_coor / 100 * CELL_COUNT_W + (int)x_coor / 100]
 
+// Math
+#define THETA(n) (n / 360.0 * 2 * M_PI)
+#define VELOCITY 5
+#define NCP 1
+
+// Guard
+#define OOB(x, y) (x < 0 || x > WINDOW_W || y < 0 || y > WINDOW_H)
+
+// Vec
 typedef struct {
   float x, y;
 } Vec2;
